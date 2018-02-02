685da6e574f98a7233f926485411ec324e39a620
Lebedev Artem <exces-st@yandex.ru>
2018-02-02 17:30:23 +0300

    replace conditoins with polymorphism in render to tree

--- src/renderers/renderToTree.js
+++ src/renderers/renderToTree.js
@@ -1,46 +1,54 @@
+import _ from 'lodash';
+
 const renderToTree = (ast) => {
-  const iter = (data, indLvl) => {
-    const indent = ' '.repeat(indLvl);
-    const indForValue = ' '.repeat(indLvl + 6);
-    const indForBrace = ' '.repeat(indLvl + 2);
+  const iter = (data, indentLvl) => {
+    const indent = ' '.repeat(indentLvl);
+    const indForValue = ' '.repeat(indentLvl + 6);
+    const indForBrace = ' '.repeat(indentLvl + 2);
 
     const objToString = (arg) => {
       const keys = Object.keys(arg);
+      const arr = keys.map(key => (arg[key] instanceof Object ? objToString(arg[key]) :
+        `${key}: ${arg[key]}`));
 
-      const pair = keys.map(key => (arg[key] instanceof Object ?
-        objToString(arg[key]) : `${key}: ${arg[key]}`));
-
-      return pair.join(`\n ${' '.repeat(indLvl + 4)} `);
+      return arr.join(`\n ${' '.repeat(indentLvl + 4)} `);
     };
 
-    const diff = (node) => {
-      if (node.type === 'changed' && node.children.length > 0) {
-        return `${indent}  ${node.key}: {\n${iter(node.children, indLvl + 4)}\n${indForBrace}}`;
-      }
-      if (node.type === 'unchanged' && node.valueAfter instanceof Object) {
-        return `${indent}  ${node.key}: {\n${indForValue}${objToString(node.valueAfter)}\n${indForBrace}}`;
-      }
-      if (node.type === 'added' && node.valueAfter instanceof Object) {
-        return `${indent}+ ${node.key}: {\n${indForValue}${objToString(node.valueAfter)}\n${indForBrace}}`;
-      }
-      if (node.type === 'removed' && node.valueBefore instanceof Object) {
-        return `${indent}- ${node.key}: {\n${indForValue}${objToString(node.valueBefore)}\n${indForBrace}}`;
-      }
-      if (node.type === 'unchanged') {
-        return `${indent}  ${node.key}: ${node.valueAfter}`;
-      }
-      if (node.type === 'added' && node.children.length === 0) {
-        return `${indent}+ ${node.key}: ${node.valueAfter}`;
-      }
-      if (node.type === 'removed' && node.children.length === 0) {
-        return `${indent}- ${node.key}: ${node.valueBefore}`;
-      }
-      if (node.type === 'updated' && node.children.length === 0) {
-        return [`${indent}- ${node.key}: ${node.valueBefore}\n${indent}+ ${node.key}: ${node.valueAfter}`];
-      }
-      return null;
+    const valueString = (value) => {
+      const stringForComplex = ` {\n${indForValue}${objToString(value)}\n${indForBrace}}`;
+      const stringForSimple = ` ${value}`;
+      return value instanceof Object ? stringForComplex : stringForSimple;
     };
-    const arr = data.map(node => diff(node));
+
+    const stringAction = [
+      {
+        string: node => `${indent}  ${node.key}: {\n${iter(node.children, indentLvl + 4)}\n${indForBrace}}`,
+        check: node => node.type === 'changed',
+      },
+      {
+        string: node => `${indent}  ${node.key}:${valueString(node.valueAfter)}`,
+        check: node => node.type === 'unchanged',
+      },
+      {
+        string: node => `${indent}+ ${node.key}:${valueString(node.valueAfter)}`,
+        check: node => node.type === 'added',
+      },
+      {
+        string: node => `${indent}- ${node.key}:${valueString(node.valueBefore)}`,
+        check: node => node.type === 'removed',
+      },
+      {
+        string: node => [`${indent}- ${node.key}: ${node.valueBefore}\n${indent}+ ${node.key}: ${node.valueAfter}`],
+        check: node => node.type === 'updated',
+      },
+    ];
+
+    const getString = (arg) => {
+      const { string } = _.find(stringAction, ({ check }) => check(arg));
+      return string(arg);
+    };
+
+    const arr = data.map(node => getString(node));
     return arr.join('\n');
   };
   const output = iter(ast, 2);

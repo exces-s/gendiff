edf750f82fb82a579427669056daae7f823a0ecb
Lebedev Artem <exces-st@yandex.ru>
2018-02-02 17:48:51 +0300

    replace conditions with polymorphism in render to plain

--- src/renderers/renderToPlain.js
+++ src/renderers/renderToPlain.js
@@ -1,23 +1,41 @@
+import _ from 'lodash';
+
 const renderToPlain = (ast) => {
   const iter = (data, pathAcc) => {
     const checkComplexValue = arg => (arg instanceof Object ? 'complex value' : arg);
-    const diff = (node) => {
-      if (node.children.length > 0) {
-        const newPathAcc = `${pathAcc}${node.key}.`;
-        return iter(node.children, newPathAcc);
-      }
-      if (node.type === 'added') {
-        return `Property '${pathAcc}${node.key}' was added with value: ${checkComplexValue(node.valueAfter)}`;
-      }
-      if (node.type === 'removed') {
-        return `Property '${pathAcc}${node.key}' was removed`;
-      }
-      if (node.type === 'updated') {
-        return `Property '${pathAcc}${node.key}' was updated. From '${node.valueBefore}' to '${node.valueAfter}'`;
-      }
-      return null;
+
+    const stringAction = [
+      {
+        string: (node) => {
+          const newPathAcc = `${pathAcc}${node.key}.`;
+          return iter(node.children, newPathAcc);
+        },
+        check: node => node.children.length > 0,
+      },
+      {
+        string: node => `Property '${pathAcc}${node.key}' was added with value: ${checkComplexValue(node.valueAfter)}`,
+        check: node => node.type === 'added',
+      },
+      {
+        string: node => `Property '${pathAcc}${node.key}' was removed`,
+        check: node => node.type === 'removed',
+      },
+      {
+        string: node => `Property '${pathAcc}${node.key}' was updated. From '${node.valueBefore}' to '${node.valueAfter}'`,
+        check: node => node.type === 'updated',
+      },
+      {
+        string: () => '',
+        check: node => node.type === 'unchanged',
+      },
+    ];
+
+    const getString = (arg) => {
+      const { string } = _.find(stringAction, ({ check }) => check(arg));
+      return string(arg);
     };
-    const result = data.map(node => diff(node)).filter(v => v);
+
+    const result = data.map(node => getString(node)).filter(v => v);
     return result.join('\n');
   };
   const output = iter(ast, '');

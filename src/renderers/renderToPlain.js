06d2b5fce3c4b9554633a54f93c3ebf1676d225e
Lebedev Artem <exces-st@yandex.ru>
2018-02-03 11:11:55 +0300

    update plain render structure

--- src/renderers/renderToPlain.js
+++ src/renderers/renderToPlain.js
@@ -1,36 +1,21 @@
-import _ from 'lodash';
-
 const renderToPlain = (ast) => {
   const iter = (data, pathAcc) => {
     const checkComplexValue = arg => (arg instanceof Object ? 'complex value' : arg);
-    const stringAction = [
-      {
-        nested: (node) => {
-          const newPathAcc = `${pathAcc}${node.key}.`;
-          return iter(node.children, newPathAcc);
-        },
+    const stringAction = {
+      nested: (node) => {
+        const newPathAcc = `${pathAcc}${node.key}.`;
+        return iter(node.children, newPathAcc);
       },
-      {
-        added: node =>
-          `Property '${pathAcc}${node.key}' was added with value: ${checkComplexValue(node.valueAfter)}`,
-      },
-      {
-        removed: node =>
-          `Property '${pathAcc}${node.key}' was removed`,
-      },
-      {
-        updated: node =>
-          `Property '${pathAcc}${node.key}' was updated. From '${node.valueBefore}' to '${node.valueAfter}'`,
-      },
-      {
-        unchanged: () => '',
-      },
-    ];
-
-    const getString = (arg) => {
-      const obj = _.find(stringAction, object => object[arg.type]);
-      return obj[arg.type](arg);
+      added: node =>
+        `Property '${pathAcc}${node.key}' was added with value: ${checkComplexValue(node.valueAfter)}`,
+      removed: node =>
+        `Property '${pathAcc}${node.key}' was removed`,
+      updated: node =>
+        `Property '${pathAcc}${node.key}' was updated. From '${node.valueBefore}' to '${node.valueAfter}'`,
+      unchanged: () => '',
     };
+
+    const getString = arg => stringAction[arg.type](arg);
 
     const result = data.map(node => getString(node)).filter(v => v);
     return result.join('\n');
